497f9e72a2c0b8daf53642ea3c23d8005741a503
peter5600 <peter.colquhoun3@gmail.com>
2021-03-10 16:13:01 +0000

    Added stuff

--- src/main/java/com/example/demo/services/PhoneBookService.java
+++ src/main/java/com/example/demo/services/PhoneBookService.java
@@ -1,5 +1,51 @@
 package com.example.demo.services;
 
-public class PhoneBookService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+import com.example.duckdemo.data.model.Duck;
+import com.example.duckdemo.dto.DuckDTO;
+import com.example.duckdemo.exceptions.DuckNotFoundException;
+
+@Service
+public class PhoneBookService {
+	
+	private PhoneRepository phoneRepository;
+	
+	private PhoneMapper phoneMapper;
+	
+	@Autowired
+	public phoneService(PhoneRepository phoneRepository, PhoneMapper phoneMapper) {
+		this.phoneRepository = phoneRepository;
+		this.phoneMapper = phoneMapper;
+	}
+	
+	public List<PhoneDTO> readAllPhones() {
+		
+	}
+	
+	public PhoneDTO readById(Integer id) {
+		
+	}
+	
+	public PhoneDTO readByName(String name) {
+		
+	}
+	
+	public PhoneDTO createPhone(Phone phone) {
+		
+	}
+	
+	public PhoneDTO updatePhone(Integer id, Phone phone) throws EntityNotFoundException {
+		
+	}
+	
+	public boolean deletePhone(Integer id) {
+		
+	}
 }
