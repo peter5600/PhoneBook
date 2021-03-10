43c44038ab11c20caa33b44c8d95c4160b379797
peter5600 <peter.colquhoun3@gmail.com>
2021-03-10 15:55:45 +0000

    Setup the mapper

---
+++ src/main/java/com/example/demo/mappers/PhoneMapper.java
@@ -0,0 +1,24 @@
+package com.example.demo.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
+public class PhoneMapper {
+
+private ModelMapper modelMapper;
+	
+	@Autowired
+	public PhoneMapper(ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+	}
+	
+	public PhoneDTO mapToDTO(Phone phone) {
+		return this.modelMapper.map(phone, PhoneDTO.class);
+	}
+	
+	public phone mapToDuck(PhoneDTO phoneDTO) {
+		return this.modelMapper.map(phoneDTO, phone.class);
+	}
+}
